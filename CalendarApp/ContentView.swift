9be11eefd755d8442a271cd01c4dd966641da81f
MasashiAkahori <il_pendolino.masa@docomo.ne.jp>
2020-08-24 01:32:36 +0900

    Initial Commit

---
+++ CalendarApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalendarApp
+//
+//  Created by 赤堀雅司 on 24/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
